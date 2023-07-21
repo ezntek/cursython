ab27a5e5d4670a25f5b58751e17500f75a9b450c
ezntek <easontek@xflymusic.com>
2023-07-21 20:49:48 +0800

    add batch transpilation

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::{
     fmt::Display,
     fs::File,
     io::{self, BufReader},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 #[derive(Debug)]
@@ -42,19 +42,33 @@ impl Display for TranspileError {
     }
 }
 
-pub fn transpile_file<P: ToOwned<Owned = PathBuf>>(
-    infile: P,
-    outdir: P,
-) -> Result<(), Box<dyn Error>> {
-    let infile = infile.to_owned();
-    let outdir = if !outdir.to_owned().exists() {
+pub fn transpile_file<P: AsRef<Path>>(infile: P, outdir: P) -> Result<(), Box<dyn Error>> {
+    let infile = if let Some(ext) = infile.as_ref().extension() {
+        if ext != "json" {
+            println!(
+                "not transpiling {}: file extension doesn't end in .json, skipping.",
+                infile.as_ref().display()
+            );
+            return Ok(());
+        }
+
+        infile.as_ref()
+    } else {
+        println!(
+            "not transpiling {}: file doesnt have an extension, skipping.",
+            infile.as_ref().display()
+        );
+        return Ok(());
+    };
+
+    let outdir = if !outdir.as_ref().exists() {
         let err = io::Error::new(
             io::ErrorKind::NotFound,
-            format!("the path at {} does not exist", outdir.to_owned().display()),
+            format!("the path at {} does not exist", outdir.as_ref().display()),
         );
         return Err(Box::new(err));
     } else {
-        outdir.to_owned()
+        outdir.as_ref()
     };
 
     let infile_file = File::open(&infile)?;
@@ -71,6 +85,35 @@ pub fn transpile_file<P: ToOwned<Owned = PathBuf>>(
     };
 
     file.write_file(Some(&outdir));
+
+    Ok(())
+}
+
+pub fn batch_transpile<P: AsRef<Path>>(in_dir: P, out_dir: P) -> Result<(), Box<dyn Error>> {
+    fn transpile_dir(in_dir: &Path, outdir: &Path) -> Result<(), Box<dyn Error>> {
+        let needed_files_iter = in_dir
+            .iter()
+            .filter(|file_or_dir| !PathBuf::from(file_or_dir).is_dir())
+            .map(|file| PathBuf::from(file));
+
+        for f in needed_files_iter {
+            transpile_file(&f, &f.join(outdir))?
+        }
+
+        Ok(())
+    }
+
+    let needed_dirs_iter = in_dir
+        .as_ref()
+        .iter()
+        .filter(|file_or_dir| PathBuf::from(file_or_dir).is_dir())
+        .map(|dir| PathBuf::from(dir));
+
+    for d in needed_dirs_iter {
+        transpile_dir(&d, &out_dir.as_ref().join(&d.file_name().unwrap()))?
+    }
+
+    transpile_dir(Path::new("./"), out_dir.as_ref())?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
-use clap::Parser;
-use cursython::*;
+use clap::{Parser, Subcommand};
+use cursython::{batch_transpile, transpile_file};
+
+#[derive(Subcommand)]
+pub enum Command {
+    Compile {
+        #[arg(short = 'o')]
+        outdir: Option<PathBuf>,
+        infile: PathBuf,
+    },
+    BatchCompile {
+        outdir: PathBuf,
+        indir: PathBuf,
+    },
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 struct Args {
-    #[arg(short = 'o')]
-    outdir: Option<PathBuf>,
-    infile: PathBuf,
+    #[command(subcommand)]
+    command: Command,
 }
 
 fn main() {
     color_eyre::install().unwrap();
 
-    let args = Args::parse();
-    transpile_file(args.infile, args.outdir.unwrap_or(PathBuf::from("./"))).unwrap();
+    match Args::parse().command {
+        Command::Compile { outdir, infile } => {
+            if !infile.exists() {
+                panic!("the file at {} doesn't exist.", infile.display())
+            }
+
+            transpile_file(infile, outdir.unwrap_or(PathBuf::from("./"))).unwrap();
+        }
+        Command::BatchCompile { outdir, indir } => {
+            if !outdir.exists() {
+                fs::create_dir(&outdir).unwrap();
+            };
+
+            batch_transpile(indir, outdir).unwrap();
+        }
+    };
 }
